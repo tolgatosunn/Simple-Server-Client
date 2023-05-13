9c550257d3f7619cae05030bdb7a4d6f2bc58b99
Tolga Tosun <73075616+tolgatosunn@users.noreply.github.com>
2023-05-13 21:41:26 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -27,27 +27,47 @@ config.set('setting', 'host', '127.0.0.1')
 config.set('setting', 'port', '9090')
 config.set('setting', 'buffer', '4096')
 
+
+# user input for client side
+config.add_section('client')
+#userinput = "{'Test': 1, 'Data': 2, 'Sample': 3}"
+userinput = "test1.txt" # content is dictionary
+#userinput = "test2.txt" # content is string
+#userinput = "UoL_logo.jpg"
+config.set('client', 'userinput', userinput)
+config.set('client', 'encryption', 'True')
+#pickling_format = 'binary'
+#pickling_format = 'json'
+pickling_format = 'xml'
+#pickling_format = 'YAML' # for testing
+config.set('client', 'format', pickling_format)
+
+
 # user input for server side
 config.add_section('server')
 config.set('server', 'print', 'True')
 config.set('server', 'save', 'True')
-filename = 'received.txt'
-#filename = 'received.json'
-#filename = 'received.pickle'
-#filename = 'received.xml'
+
+
+'''
+This if clause sets filename variable for the server module.
+'''
+if userinput[-4:]=='.txt':
+    filename = 'received.txt'
+elif isinstance(eval(userinput), dict):
+    if pickling_format=='binary':
+        filename ='received.pickle'
+    elif pickling_format=='json':
+        filename ='received.json'
+    elif pickling_format=='xml':
+        filename ='received.xml'
+    else:
+        filename = 'received.yaml'
+else:
+    filename = userinput
+
 config.set('server', 'file', filename)
 
-# user input for client side
-config.add_section('client')
-userinput = "{'Test': 1, 'Data': 2, 'Sample': 3}"
-#userinput = "test1.txt" # content is dictionary
-#userinput = "test2.txt" # content is string
-config.set('client', 'userinput', userinput)
-config.set('client', 'encryption', 'True')
-pickling_format = 'binary'
-#pickling_format = 'json'
-#pickling_format = 'xml'
-config.set('client', 'format', pickling_format)
 
 # write the new structure to the new file
 filename = 'configfile.ini'
